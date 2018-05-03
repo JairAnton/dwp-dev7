22d14977bb2a8939a57fc2a6a810ad2348f498bb
Unknown <zerodeckvc@hotmail.com>
2018-05-02 19:53:28 -0500

    HU203_Opportunity: Manual Creation
    Componente quickAction, Etiquetas, Traducciones,Clases, Clases de  prueba, Layout para Accounts

---
+++ src/aura/cQuickAction_NewOpportunity/cQuickAction_NewOpportunityController.js
@@ -0,0 +1,24 @@
+({
+	init : function(component, event, helper) {		
+		//Static Labels
+		component.set("v.lblTitle", $A.get("$Label.c.New_Opportunity"));
+		component.set("v.lblAccountName", $A.get("!$ObjectType.Account.fields.Name.Label"));
+		component.set("v.lblOpportunityName", $A.get("$Label.c.OpportunityName"));
+		component.set("v.lblPlanningDate", $A.get("$Label.c.PlannigDate"));
+		component.set("v.lblCloseDate", $A.get("$Label.c.CloseDate"));
+		component.set("v.lblAmount", $A.get("$Label.c.Amount"));
+		component.set("v.lblCurrency", $A.get("$Label.c.Currency"));
+		component.set("v.lblComments", $A.get("$Label.c.Comments"));	
+		component.set("v.lblProbability", $A.get("$Label.c.Probability"));	
+		
+	},
+	 toggleDialog : function(component, event, helper) {
+        $A.get("e.force:closeQuickAction").fire();
+    },
+    cSaveAction: function(component, event, helper) {
+	    helper.close(component, event, helper);
+	    helper.restSaveRecord(component, event, helper);
+    }
+
+	
+})
