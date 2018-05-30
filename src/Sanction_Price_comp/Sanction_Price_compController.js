d763630fc134e2e500f6ee7d4e5d611587bc2220
Unknown <zerodeckvc@hotmail.com>
2018-05-30 12:42:34 -0500

    Fixes HU431,432,433,427

---
+++ src/Sanction_Price_comp/Sanction_Price_compController.js
@@ -0,0 +1,25 @@
+({
+	init: function(component, event, helper) {
+		component.set("v.lblComments", $A.get("$Label.c.Comments"));
+		component.set("v.lblCancel", $A.get("$Label.c.Cancel"));
+	    helper.bringData(component, event, helper); 
+    },
+	close : function(component, event, helper) {
+		helper.destroyCmp(component, event, helper);
+	},
+	sanction : function(component, event, helper) {
+		component.set('v.sanAction',event.getSource().get("v.name"));
+		component.find("btnContinue").set("v.disabled", false);
+	},
+	continue : function(component, event, helper) {
+		helper.sanctionActions(component, event, helper);
+	},
+	desestimar : function(cmp, event, helper) {	
+		var inputObject=cmp.get('v.inputAttributes');  
+	    var compEvent = cmp.getEvent('dynamicFlowWizardContinue');
+	    compEvent.setParams({'inputAttributes': inputObject, 'nextComponent':'c:submitToDiscardModal_comp' });
+	    compEvent.fire();
+	    
+	}
+
+})
