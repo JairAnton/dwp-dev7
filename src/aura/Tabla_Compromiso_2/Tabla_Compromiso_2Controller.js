b6cf0c5fd58788169be8e70cb577ca3242b4e600
Arsenio Perez <arseniopl1@gmail.com>
2018-06-01 12:10:25 -0500

    Tarifario 433 final
    TArid

--- src/aura/Tabla_Compromiso_2/Tabla_Compromiso_2Controller.js
+++ src/aura/Tabla_Compromiso_2/Tabla_Compromiso_2Controller.js
@@ -4,12 +4,10 @@
         action.setParams({
             "Filtro":cmp.get("v.recordId") 
         });
-        
         action.setCallback(this, $A.getCallback(function (response) {
             var state = response.getState();
             if (state === "SUCCESS") {
                 cmp.set("v.mydata", response.getReturnValue());
-                
             } else if (state === "ERROR") {
                 var errors = response.getError();
                 console.error(errors);
