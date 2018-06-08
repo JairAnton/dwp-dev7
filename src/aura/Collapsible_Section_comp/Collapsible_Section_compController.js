59889991875d0cc13bf27bb81eefbd8544b855e7
Unknown <zerodeckvc@hotmail.com>
2018-06-07 19:45:13 -0500

    HU 451,452,453,455,456,457,FixNewOpp,FixOPPLayout

---
+++ src/aura/Collapsible_Section_comp/Collapsible_Section_compController.js
@@ -0,0 +1,21 @@
+({
+    sectionZero : function(component, event, helper) {
+       helper.helperFun(component,event,'articleZero');
+    },
+
+    sectionOne : function(component, event, helper) {
+       helper.helperFun(component,event,'articleOne');
+    },
+    
+   sectionTwo : function(component, event, helper) {
+      helper.helperFun(component,event,'articleTwo');
+    },
+   
+   sectionThree : function(component, event, helper) {
+      helper.helperFun(component,event,'articleThree');
+   },
+   
+   sectionFour : function(component, event, helper) {
+      helper.helperFun(component,event,'articleFour');
+   },
+})
