b82181c29d8b51f39e9e0973b75cf0905b592a2f
ismael.nunez.garcia <ismael.nunez.garcia@accenture.com>
2018-08-03 10:28:26 +0200

    fix sanction price

--- src/aura/ProductPriceViewer_cmp/ProductPriceViewer_cmpHelper.js
+++ src/aura/ProductPriceViewer_cmp/ProductPriceViewer_cmpHelper.js
@@ -8,16 +8,16 @@
             var state = response.getState();
             if (state === "SUCCESS") {
                 var ret = response.getReturnValue();
-                if(ret.hasOLI){
+                if(ret.hasOLI && ret.priceIndicator){
                     var objectInput = {
                         'IdOppLineItem':ret.IdOppLineItem,
                         'approvalMethod':ret.approvalMethod
                     };
                     cmp.set('v.objectInput',objectInput);
                 }
-
-                cmp.set('v.isLoad',true);
+                cmp.set('v.priceIndicator',ret.priceIndicator);
                 cmp.set('v.hasOLI',ret.hasOLI);
+                cmp.set('v.isLoad',true);
             }
         }); 
         $A.enqueueAction(action);
