1c5e1ae7d40631b8c18ffdd3a99b4f47627f064f
adrian.caro.contractor <adrian.caro.contractor@bbva.com>
2018-09-17 18:02:00 +0200

    SanctionPrice

--- src/aura/SanctionPrice_cmp/SanctionPrice_cmpController.js
+++ src/aura/SanctionPrice_cmp/SanctionPrice_cmpController.js
@@ -1,14 +1,14 @@
 ({
-	close : function(component, event, helper) {
-	 	helper.closeMe(component, event, helper);
-	},
-	doContinue: function(cmp, evt, helper){
-		helper.continue(cmp, evt, helper);
-	},
-	doInit: function(cmp, evt, helper){
-		helper.getInfo(cmp, evt, helper);
-	},
-	saveResponse: function(cmp, evt, helper){
-		helper.doNextComponent(cmp, evt, helper);
-	}
+    close : function(component, event, helper) {
+        helper.closeMe(component, event, helper);
+    },
+    doContinue: function(cmp, evt, helper){
+        helper.continue(cmp, evt, helper);
+    },
+    doInit: function(cmp, evt, helper){
+        helper.getInfo(cmp, evt, helper);
+    },
+    saveResponse: function(cmp, evt, helper){
+        helper.doNextComponent(cmp, evt, helper);
+    }
 })
