28b2f0cb289b7466eafd78d0eb5f16e5616456f5
Unknown <zerodeckvc@hotmail.com>
2018-10-21 17:16:51 -0500

    HT935
    Account Resume Info Sanction Price

---
+++ src/aura/AccountResumeInfo_comp/AccountResumeInfo_compHelper.js
@@ -0,0 +1,26 @@
+({
+    getInfo : function(component, event, helper){
+      /*  var idOpp = cmp.get('v.recordId');
+        var action = cmp.get("c.getInfo");
+        action.setParams({
+            "recordId" : inputObject.recordId
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var ret = response.getReturnValue();
+                var objectInput = {
+                    'IdOppLineItem':ret.IdOppLineItem,
+                    'approvalMethod':ret.approvalMethod,
+                    'dinamicInput':'-'
+                };
+                if(ret.approvalMethod == 'Tarifario' && ret.dynamicValue!=undefined){
+                    objectInput['dinamicInput'] = ret.dynamicValue.toString() + ',-';
+                }
+                cmp.set('v.objectInput',objectInput);
+                cmp.set('v.isLoad',true);
+            }
+        }); 
+        $A.enqueueAction(action);*/
+    }
+})
