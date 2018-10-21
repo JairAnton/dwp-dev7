12c74b74168de2643536838530d6befd3ba7ece2
Unknown <zerodeckvc@hotmail.com>
2018-10-21 18:11:46 -0500

    FixSONAR bugs

--- src/aura/AccountResumeInfo_comp/AccountResumeInfo_compController.js
+++ src/aura/AccountResumeInfo_comp/AccountResumeInfo_compController.js
@@ -1,11 +1,12 @@
 ({
 	init:  function(component,event,helper){
-		helper.getInfo(component, event, helper);
+		helper.showGrafica(component,event);
 		component.set("v.bGrafica",true);
 	},
 	filtroGrafica : function(component,event,helper){
 		component.set("v.sFiltro",component.find('selFiltro').get('v.value'));		
         component.set("v.bGrafica",false);
-        component.set("v.bGrafica",true);
-    }
+       	helper.showGrafica(component,event);
+    },
+    
 })
