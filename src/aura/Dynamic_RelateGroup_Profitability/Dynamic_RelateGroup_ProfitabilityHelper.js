eb48f87365938c12ed0712e8a4153947790ecf35
Arsenio Perez <arseniopl1@gmail.com>
2018-10-23 00:12:18 -0500

    NEW VARIABLE

---
+++ src/aura/Dynamic_RelateGroup_Profitability/Dynamic_RelateGroup_ProfitabilityHelper.js
@@ -0,0 +1,26 @@
+({
+	ConsultaProducto_helper : function(component, event) {
+        var items = [];
+		var opp = component.get("v.recordId");
+        var action = component.get("c.RecuperaServicio");
+        action.setParams({
+            "IdAccount" : opp
+        });  
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var arr = response.getReturnValue() ;
+                arr.forEach(function(element) {
+                    items.push({ value: element, label: element });
+                });
+                component.set("v.options", items);
+            }
+        });
+        $A.enqueueAction(action);
+	},
+    BtnNeutral: function(component, event, helper){
+        component.find("BtnTotal").set("v.variant", "Neutral");
+        component.find("BtnPEN").set("v.variant", "Neutral");
+        component.find("BtnUSD").set("v.variant", "Neutral");
+    }
+})
