0212ddb2f122756f9c23d91e7ee61d900ac26e7d
Arsenio Perez <arseniopl1@gmail.com>
2018-10-26 16:55:11 -0500

    Modificacion Account Profitability

--- src/aura/Dynamic_RelateGroup_Profitability/Dynamic_RelateGroup_ProfitabilityController.js
+++ src/aura/Dynamic_RelateGroup_Profitability/Dynamic_RelateGroup_ProfitabilityController.js
@@ -2,49 +2,39 @@
     init : function(component, event, helper) {
         component.set('v.showSpinner',false);
         helper.ConsultaProducto_helper(component, event);
-        component.set('v.isLoad', false);
-        component.set('v.ProductIS', 'true');
-        component.set('v.coint1', 'PEN');
         component.find("BtnTotal").set("v.variant", "brand");
-        component.set("v.bGrafica",true);
     },
     BtnChangTotal: function(component, event, helper){
-        var Type='TOTAL';
-        component.set('v.profiType', Type);
-        helper.BtnNeutral(component, event, helper);
-        component.find("BtnTotal").set("v.variant", "brand");
-        component.set('v.coint1', 'PEN');
+        helper.BtnNeutral(component, event, helper,'TOTAL','BtnTotal','PEN');
         $A.get('e.force:refreshView').fire();
         component.set("v.bGrafica",true);
     },
     BtnChangPEN: function(component, event, helper){
-        var Type='MN';
-        component.set('v.profiType', Type);
-        helper.BtnNeutral(component, event, helper);
-        component.find("BtnPEN").set("v.variant", "brand");
-        component.set('v.coint1', 'PEN');
+        helper.BtnNeutral(component, event, helper,'MN','BtnPEN','PEN');
         $A.get('e.force:refreshView').fire();
         component.set("v.bGrafica",true);
     },
     BtnChangUSD: function(component, event, helper){
-        var Type='ME';
-        component.set('v.profiType', Type);
-        helper.BtnNeutral(component, event, helper);
-        component.find("BtnUSD").set("v.variant", "brand");
-        component.set('v.coint1', 'USD');
+        helper.BtnNeutral(component, event, helper,'ME','BtnUSD','USD');
         $A.get('e.force:refreshView').fire();
         component.set("v.bGrafica",true);
     },
     chgProduct: function(component, event, helper){
         component.set("v.ProductIS", event.getParam("value"));
+            helper.toTalClient(component, event, helper);
+        if(event.getParam("value")!=='CLIENTE'){
+        	helper.BtnNeutral(component, event, helper,'TOTAL','BtnTotal','PEN');
+        }
         $A.get('e.force:refreshView').fire();
         component.set("v.bGrafica",true);
     },
     doInitRefreshView : function(component, event, helper) {
         component.set('v.isLoad', false);
+        component.set("v.bGrafica",false);
         if(window.location.href.includes(component.get('v.recordId'))){
             component.set('v.isLoad', true);
             component.set("v.bGrafica",true);
         }
     }
+    
 })
