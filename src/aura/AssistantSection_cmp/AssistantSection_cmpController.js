f85e08139701e531b085850637af29400918c346
jose <juliooncebaylopez@gmail.com>
2018-12-10 11:17:42 -0500

    101218

--- src/aura/AssistantSection_cmp/AssistantSection_cmpController.js
+++ src/aura/AssistantSection_cmp/AssistantSection_cmpController.js
@@ -2,7 +2,11 @@
 	doInitRefreshView : function(component, event, helper) {
         if(window.location.href.includes(component.get('v.recordId'))){
 			component.set('v.isLoad', false);
-			component.set('v.isLoad', true);
+            //component.set('v.isLoad', true);
+            helper.showRefreshView(component,event);
         }
+    },
+    showRefreshView : function(component,event){
+        component.set('v.isLoad', true);
     }
  })
