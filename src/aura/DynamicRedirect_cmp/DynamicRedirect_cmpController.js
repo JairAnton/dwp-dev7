441832641ed6877233b1f62cf5c2e783e158bf3d
Arsenio Perez <arseniopl1@gmail.com>
2018-12-19 12:18:42 -0500

    Codsmel

--- src/aura/DynamicRedirect_cmp/DynamicRedirect_cmpController.js
+++ src/aura/DynamicRedirect_cmp/DynamicRedirect_cmpController.js
@@ -2,10 +2,10 @@
     jsLoaded : function(cmp, evt, helper) {
         var lstTopics = cmp.get('v.nameTopic').split(',');
         var jsonResponse = null;
-        var actionSessionId = cmp.get("c.getSessionId"); 		
+        var actionSessionId = cmp.get("c.getSessionId");
         actionSessionId.setCallback(this, function(response) {
             if (response.getState() === "SUCCESS") {
-                jsonResponse = JSON.parse(response.getReturnValue());                     
+                jsonResponse = JSON.parse(response.getReturnValue());
                 (function($){
                     $.cometd.init({
                         url: window.location.protocol+'//'+window.location.hostname+'/cometd/29.0/',
@@ -20,7 +20,9 @@
                             var objPos = cmp.get('v.objMap');
                             var pos = objPos[message.channel];
                             var strApiNameCompare = lstStrApiName[pos];
-                            if ( (jsonResponse.userId ===  message.data.sobject.CreatedById || jsonResponse.userId ===  message.data.sobject.OwnerId) && cmp.get('v.recordId') === message.data.sobject[strApiNameCompare] ){
+                            if ( (jsonResponse.userId ===  message.data.sobject.CreatedById || 
+                                  jsonResponse.userId ===  message.data.sobject.OwnerId) && 
+                                  cmp.get('v.recordId') === message.data.sobject[strApiNameCompare] ){
                                 var navEvt = $A.get("e.force:navigateToSObject");
                                 navEvt.setParams({
                                     "recordId": message.data.sobject.Id,
@@ -31,7 +33,7 @@
                         } ); // 2018/11/27 - 16:33 CORRECCION DEUDA TECNICA Ernesto
                     }
                     cmp.set('v.objMap',objToMap);
-                })(jQuery);  
+                })(jQuery);
             }
         });
         $A.enqueueAction(actionSessionId);
