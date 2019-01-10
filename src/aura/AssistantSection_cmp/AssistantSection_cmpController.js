0e359adf16c5220d66d7653f1c3030504ef4632a
Arsenio Perez <arseniopl1@gmail.com>
2019-01-10 10:29:07 -0500

    User secction

--- src/aura/AssistantSection_cmp/AssistantSection_cmpController.js
+++ src/aura/AssistantSection_cmp/AssistantSection_cmpController.js
@@ -2,11 +2,7 @@
 	doInitRefreshView : function(component, event, helper) {
         if(window.location.href.includes(component.get('v.recordId'))){
 			component.set('v.isLoad', false);
-            //component.set('v.isLoad', true);
-            showRefreshView(component,event);
+            helper.showRefreshView(component,event);
         }
-    },
-    showRefreshView : function(component,event){
-        component.set('v.isLoad', true);
     }
  })
