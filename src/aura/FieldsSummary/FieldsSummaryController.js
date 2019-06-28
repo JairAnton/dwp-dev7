28cd523ebac2c1665fec5dd28366fa038bc8a0ce
Martin100 <martin.mori.contractor@bbva.com>
2019-06-28 11:56:25 -0500

    MAXMIX2-1099

--- src/aura/FieldsSummary/FieldsSummaryController.js
+++ src/aura/FieldsSummary/FieldsSummaryController.js
@@ -1,16 +1,32 @@
 ({
-	doContinue : function(cmp, evt, helper) {
+    doInit : function(cmp, evt, helper){
+        var action = cmp.get("c.getInfo");
+        action.setParams({
+            "recordId" : cmp.get('v.recordId'), "isOkLabel" : 'isOk'
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+				var ret = response.getReturnValue();
+			    if(ret.isOk){
+                    cmp.set('v.values',ret.values);
+                    cmp.set('v.isOk',true);
+				}else{
+					cmp.set('v.isOk',false);
+				}
+            }
+        });
+        $A.enqueueAction(action);
+	}, doContinue : function(cmp, evt, helper) {
         var storeHTML = document.getElementById('storeHTML');
         var toastEvent = $A.get("e.force:showToast");
+        var msg = $A.get("$Label.c.PriceApprovalLetterCredit");
         toastEvent.setParams({
-            "title": "Success!",
-            "message": 'Operación exitosa',
-            "type": "success"
+            "title": "Success!", "message": msg, "type": "success"
         });
         var action = cmp.get("c.doSave");
         action.setParams({
-            "recordId" : cmp.get('v.OpportunityId'),
-            "storeHtml" : storeHTML.innerHTML
+            "recordId" : cmp.get('v.OpportunityId'), "storeHtml" : storeHTML.innerHTML, "isOkLabel" : 'isOk'
         });
         action.setCallback(this, function(response) {
             var state = response.getState();
