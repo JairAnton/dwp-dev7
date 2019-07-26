448128cb2510b5eac2d3f45b72b6a26e2f440d6b
Diego Carbajal <diego.carbajal.contractor@bbva.com>
2019-07-26 11:48:05 -0500

    MAXMIX2-1086

--- src/aura/SanctionPriceDecisionAnalist_cmp/SanctionPriceDecisionAnalist_cmpController.js
+++ src/aura/SanctionPriceDecisionAnalist_cmp/SanctionPriceDecisionAnalist_cmpController.js
@@ -5,13 +5,13 @@
 	doInit : function(component, event, helper) {
 		helper.getInfo(component, event, helper);
 	},
-	classChange : function(cmp, evt, helper){
+	classChange : function(cmp, evt, helper) {
         var objSetup = cmp.get('v.objSetup');
 		objSetup['btnSelectConfig'] = objSetup['getInfoButtons'].lstButtons[parseInt(evt.target.id)];
 		cmp.set('v.objSetup',objSetup);
 		cmp.set('v.enableContinue',true);
 	},
-	doContinue : function(cmp, evt, helper){
+	doContinue : function(cmp, evt, helper) {
 		helper.doContinue(cmp, evt, helper);
 	}
 })
