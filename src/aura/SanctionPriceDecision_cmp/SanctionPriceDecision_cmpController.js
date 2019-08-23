307ece83f0bb13bdfac40236cb8dd5b3ce7ecd2f
Martin100 <martin.mori.contractor@bbva.com>
2019-08-23 12:31:20 -0500

    MAXMIX2-1158

--- src/aura/SanctionPriceDecision_cmp/SanctionPriceDecision_cmpController.js
+++ src/aura/SanctionPriceDecision_cmp/SanctionPriceDecision_cmpController.js
@@ -9,17 +9,20 @@
         var objSetup = cmp.get('v.objSetup');
 		objSetup['btnSelectConfig'] = objSetup['getInfoButtons'].lstButtons[parseInt(evt.target.id)];
 		cmp.set('v.objSetup',objSetup);
-		var fieldsForm = cmp.find('table');
-        var lstFields = [];
-        var outputs = fieldsForm.find('outputHTML');
-        if(outputs.length !== undefined) {
-            for(var i in outputs) {
-                var inputObject = outputs[i].get('v.fieldObject');
-                lstFields.push(helper.htmlObject(inputObject, evt));
+        var inputAtt = cmp.get('v.inputAttributes');
+        if(inputAtt.approvalMethod === 'Web') {
+            var fieldsForm = cmp.find('table');
+            var lstFields = [];
+            var outputs = fieldsForm.find('outputHTML');
+            if(outputs.length !== undefined) {
+                for(var i in outputs) {
+                    var inputObject = outputs[i].get('v.fieldObject');
+                    lstFields.push(helper.htmlObject(inputObject, evt));
+                }
             }
+            fieldsForm.set('v.fieldObject',lstFields);
+            cmp.set('v.refreshComp',true);
         }
-        fieldsForm.set('v.fieldObject',lstFields);
-        cmp.set('v.refreshComp',true);
         cmp.set('v.enableContinue',true);
 	},
 	doContinue : function(cmp, evt, helper) {
