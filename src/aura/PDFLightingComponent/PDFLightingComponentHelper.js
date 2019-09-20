00ab753171e5436cd88a000694d943bfdfc55715
Diego Carbajal <diego.carbajal.contractor@bbva.com>
2019-09-20 12:52:30 -0500

    MAXMIX2-1025 - RELEASE FORMALIZATION

--- src/aura/PDFLightingComponent/PDFLightingComponentHelper.js
+++ src/aura/PDFLightingComponent/PDFLightingComponentHelper.js
@@ -2,20 +2,52 @@
     closeMe : function(component, event) {
 		component.destroy();
 	},
-    PDF : function(component, event) {
+    reasignCase : function(component, event, helper) {
+        var action = component.get('c.reasignCase');
+        action.setParams({"recordId" : component.get('v.inputAttributes.recordId')});
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === "SUCCESS") {
+                var res = response.getReturnValue();
+                if(!res.isError) {
+                    helper.msgAndClose(component, event, helper, res.msgSuccess, 'success', true);
+                } else {
+                    helper.msgAndClose(component, event, helper, res.msgError, '', true);
+                }
+            } else {
+                helper.msgAndClose(component, event, helper, false, false, true);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    PDF : function(component, event, helper) {
         var inputObject = component.get('v.inputAttributes');
         var save_action = component.get("c.PDF_formalization");
         save_action.setParams({recordId : inputObject.recordId});
         save_action.setCallback(this, function(response) {
-         var state = response.getReturnValue();
-       if (state === "SUCCESS") {       
-        $A.get("e.force:navigateToURL");//Ernesto: correcion
-           window.open('/apex/PDF_formalizacion_vfp?Id='+inputObject.recordId);
-            component.destroy();
-         }else{
-                alert(state);
-           }
+            var state = response.getReturnValue();
+            if (state === "SUCCESS") {
+                $A.get("e.force:navigateToURL");//Ernesto: correcion
+                window.open('/apex/PDF_formalizacion_vfp?Id='+inputObject.recordId);
+                $A.get('e.force:refreshView').fire();
+                component.destroy();
+            } else {
+                helper.msgAndClose(component, event, helper, '', '', true);
+            }
         });
         $A.enqueueAction(save_action);
+    },
+    msgAndClose : function(component, event, helper, message, type, close) {
+        var toastEvent = $A.get("e.force:showToast");
+        var message = (message ? message : $A.get("$Label.c.Dwp_msgGenericError"));
+        toastEvent.setParams({
+            "message": message,
+            "type": (type ? type : "error")
+        });
+        toastEvent.fire();
+        $A.get('e.force:refreshView').fire();
+        if(close) {
+            this.destroyCmp(component, event, helper);
+        }
     }
 })
--- src/aura/btnProductUnit_cmp/btnProductUnit_cmpController.js
+++ src/aura/btnProductUnit_cmp/btnProductUnit_cmpController.js
@@ -1,28 +1,49 @@
 ({
 	doInit : function(component, event, helper) {
-		var action=component.get("c.sendProductUnit");
-        action.setParams({"oppId" : component.get("v.inputAttributes.recordId")});
+		var action = component.get('c.validateSGof');
+        action.setParams({'oppId' : component.get('v.inputAttributes.recordId')});
         action.setCallback(this, function(response) {
             var state = response.getState();
-            var toastEvent = $A.get("e.force:showToast");
             if(state === "SUCCESS") {
                 var res = response.getReturnValue();
-                var error = res.isError ? "error" : "success";
-                toastEvent.setParams({
-                    "message": res.msg,
-                    "type": error
-        		});
+                if(!res.isError) {
+                    if(res.property === 'Empty' || res.property === 'Assigned') {
+                        component.set("v.msgProperty", res.msg);
+                        component.set("v.dialog", res.dialog);
+                        component.set("v.hasHeader", true);
+                    	component.set("v.validate", true);
+                    } else {
+                        helper.productUnit(component, event, helper);
+                    }
+                } else {
+                    helper.msgAndClose(component, event, helper, res.msgError, '', true);
+                }
             } else {
-                toastEvent.setParams({
-                    "message": $A.get("$Label.Dwp_msgGenericError"),
-                    "type": "error"
-                });
+               helper.msgAndClose(component, event, helper, '', '', true);
             }
-            toastEvent.fire();
-            $A.get('e.force:refreshView').fire();
-            helper.destroyCmp(component, event, helper);
         });
         $A.enqueueAction(action);
-	}
+	},
+    asignCase : function(component, event, helper) {
+        var action = component.get('c.reasignCaseFromOpp');
+        action.setParams({"oppId" : component.get('v.inputAttributes.recordId')});
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === "SUCCESS") {
+                var res = response.getReturnValue();
+                if(!res.isError) {
+                    helper.msgAndClose(component, event, helper, res.msgSuccess, 'success', true);
+                } else {
+                    helper.msgAndClose(component, event, helper, res.msgError, '', true);
+                }
+            } else {
+                helper.msgAndClose(component, event, helper, false, false, true);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    close : function(component, event, helper) {
+        component.destroy();
+    }
 
 })
