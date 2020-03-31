c285114796afe46c992b9b81a6eb8296bcc09536
MARTIN ALEJANDRO MORI CHAVEZ <martin.mori.contractor@bbva.com>
2020-03-31 17:06:30 -0500

    MAXMIX2-1285

--- src/lwc/bE_TableHome_Lwc/bE_TableHome_Lwc.js
+++ src/lwc/bE_TableHome_Lwc/bE_TableHome_Lwc.js
@@ -1,25 +1,36 @@
 import { LightningElement, wire, api, track } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
+import getId from '@salesforce/apex/BE_TableHome_Ctr.getId';
 import getData from '@salesforce/apex/BE_TableHome_Ctr.getData';
 import { NavigationMixin } from 'lightning/navigation';
 import LANG from '@salesforce/i18n/lang';
 
 export default class bE_TableHome_Lwc extends NavigationMixin(LightningElement) {
     lang = LANG;
-    @api idReport; /** Report id */
+    @api devReport; /** Report id */
     @api numRows; /** Num rows */
     @api title; /** Title of section */
     @api footer; /** footer of section */
     @track lastRun; /** last run */
     @track customTitle;
     @track customFooter;
-    
+    idReport;
+    @wire(getId,{
+        "reportDev": "$devReport",
+    })
+    wiredResult({ error, data }) {
+        if (data && data!==null && data!=='undefined' && data.length > 0) {
+            this.idReport = data;
+        } else if (error) {
+            this.idReport = '';
+        }
+    }
+
     @wire(getData,{
         "reportId": "$idReport",
         "limitRows": "$numRows"
     }) rows;
     keyBlock = 'keyBlock';
-    
     connectedCallback(){
         this.customTitle = this.isNotEmpty(this.title) ? JSON.parse((this.title))[this.lang] : '';
         this.customFooter = this.isNotEmpty(this.footer) ? JSON.parse((this.footer))[this.lang] : '';
@@ -27,14 +38,12 @@ export default class bE_TableHome_Lwc extends NavigationMixin(LightningElement) 
         var d = new Date();
         this.lastRun = d.toLocaleString(this.lang, options);
     }
-
     handleClick(e){
         var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: true };
         var d = new Date();
         this.lastRun = d.toLocaleString(this.lang, options);
         refreshApex(this.rows);
     }
-    
     navigateToReport(){
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
@@ -45,7 +54,6 @@ export default class bE_TableHome_Lwc extends NavigationMixin(LightningElement) 
             }
         });
     }
-
     /**VALIDATE NULL, EMPTY AND BLANK*/
     isNotEmpty(obj){
         const notEmpty = (obj === null || obj === undefined || obj === "") ? false : true;
