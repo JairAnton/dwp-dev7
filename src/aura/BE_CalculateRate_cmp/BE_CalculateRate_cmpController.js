67ef5ce43b785cba3bca7e65510439d1fbaf4ab0
Luis Vargas <>
2021-07-01 16:12:56 -0500

    SonarQ Fix

--- src/aura/BE_CalculateRate_cmp/BE_CalculateRate_cmpController.js
+++ src/aura/BE_CalculateRate_cmp/BE_CalculateRate_cmpController.js
@@ -1,20 +1,5 @@
 ({
     getCal: function (cmp, evt, helper) {
         helper.calRate(cmp, evt, helper);
-    },
-    showSpinner: function (component) {
-        component.set("v.spinner", true);
-    },
-    hideSpinner: function (component) {
-        component.set("v.spinner", false);
-    },
-    onCheckUseCommissions: function (cmp, evt, helper) {
-        helper.updateUseCommissions(cmp, evt, helper);
-    },
-    emitCommissionEvent: function (cmp, evt, helper) {
-        helper.emitEventHelper(cmp, evt, helper);
-    },
-    handlerCommissionCall: function (cmp, evt, helper) {
-        helper.handlerCommissionCallHelper(cmp, evt, helper);
     }
 })
