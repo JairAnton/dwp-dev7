299893d8af312063207e7adb2e101fbc2cdc1adc
Luis Vargas <>
2021-07-01 16:20:12 -0500

    SonarQ Fix

--- src/aura/BE_CalculateRate_cmp/BE_CalculateRate_cmpController.js
+++ src/aura/BE_CalculateRate_cmp/BE_CalculateRate_cmpController.js
@@ -1,5 +1,27 @@
 ({
-    'getCal': function (cmp, evt, helper) {
+    getCal: function (cmp, evt, helper) {
         helper.calRate(cmp, evt, helper);
+    },
+    showSpinner: function (component) {
+        component.set("v.spinner", true);
+    },
+    hideSpinner: function (component) {
+        component.set("v.spinner", false);
+    },
+    closeAlertInd: function () {
+        var alerta = document.getElementById("idAlertInd");
+        alerta.classList.add("slds-hide");
+    },
+    closeAlertMar: function () {
+        var alerta = document.getElementById("idAlertMar").classList.add("slds-hide");
+    },
+    onCheckUseCommissions: function (cmp, evt, helper) {
+        helper.updateUseCommissions(cmp, evt, helper);
+    },
+    emitCommissionEvent: function (cmp, evt, helper) {
+        helper.emitEventHelper(cmp, evt, helper);
+    },
+    handlerCommissionCall: function (cmp, evt, helper) {
+        helper.handlerCommissionCallHelper(cmp, evt, helper);
     }
-})
+});
