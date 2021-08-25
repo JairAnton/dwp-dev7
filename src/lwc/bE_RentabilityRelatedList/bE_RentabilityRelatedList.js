7d6f7848209c8f51fd979d9539799870f57170f9
Luis Vargas <>
2021-08-25 15:31:08 -0500

    test update

--- src/lwc/bE_RentabilityRelatedList/bE_RentabilityRelatedList.js
+++ src/lwc/bE_RentabilityRelatedList/bE_RentabilityRelatedList.js
@@ -3,7 +3,7 @@ import { refreshApex } from "@salesforce/apex";
 import getData from "@salesforce/apex/BE_ProfilabilityController_cls.getRDt";
 import getGrowthVariableData from "@salesforce/apex/BE_ProfilabilityController_cls.getGrowthVariableData";
 import saveRentabilityDrivers from "@salesforce/apex/BE_ProfilabilityController_cls.saveRentabilityDrivers";
-import { updateEstimationList, calculator } from "./bE_RentabilityRelatedListUtilities";
+import { updateEstimationList, calculator } from "./be_RentabilityRelatedListUtilities";
 
 let date = new Date().getFullYear();
 
