06ac2757ccc4a133f793fc10d61afcb570746129
JaimeAndreSilva <jaime.silva.contractor@bbva.com>
2021-09-07 13:17:57 -0500

    Calce Custom Dashboard (ChartJS)

---
+++ src/lwc/generateBarChart/generateBarChart.js
@@ -0,0 +1,31 @@
+import { LightningElement, api } from 'lwc';
+import ChartJS23 from '@salesforce/resourceUrl/ChartJS23';
+import { loadScript } from 'lightning/platformResourceLoader';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+ 
+export default class generateBarChart extends LightningElement {
+    @api chartConfig;
+ 
+    isChartJsInitialized;
+    renderedCallback() {
+        if (this.isChartJsInitialized) {
+            return;
+        }
+        // load chartjs from the static resource
+        Promise.all([loadScript(this, ChartJS23)])
+            .then(() => {
+                this.isChartJsInitialized = true;
+                const ctx = this.template.querySelector('canvas.barChart').getContext('2d');
+                this.chart = new window.Chart(ctx, JSON.parse(JSON.stringify(this.chartConfig)));
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error loading Chart',
+                        message: error.message,
+                        variant: 'error',
+                    })
+                );
+            });
+    }
+}
